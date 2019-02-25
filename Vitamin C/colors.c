0c6542de60a40a3515d4e827bdf28d69de1e8c73
Aescosaurus <michaelbuslik@hotmail.com>
2019-02-25 09:44:21 -0700

    Added color functions to make putpixel easier.

--- Vitamin C/colors.c
+++ Vitamin C/colors.c
@@ -11,3 +11,58 @@ color_t make_rgb( uchar r,uchar g,uchar b )
 {
 	return( SDL_MapRGB( colors_pixel_format,r,g,b ) );
 }
+
+color_t color_white()
+{
+	return( make_rgb( 255,255,255 ) );
+}
+
+color_t color_black()
+{
+	return( make_rgb( 0,0,0 ) );
+}
+
+color_t color_gray()
+{
+	return( make_rgb( 127,127,127 ) );
+}
+
+color_t color_light_gray()
+{
+	return( make_rgb( 191,191,191 ) );
+}
+
+color_t color_dark_gray()
+{
+	return( make_rgb( 63,63,63 ) );
+}
+
+color_t color_red()
+{
+	return( make_rgb( 255,0,0 ) );
+}
+
+color_t color_yellow()
+{
+	return( make_rgb( 255,255,0 ) );
+}
+
+color_t color_green()
+{
+	return( make_rgb( 0,255,0 ) );
+}
+
+color_t color_cyan()
+{
+	return( make_rgb( 0,255,255 ) );
+}
+
+color_t color_blue()
+{
+	return( make_rgb( 0,0,255 ) );
+}
+
+color_t color_magenta()
+{
+	return( make_rgb( 255,0,255 ) );
+}
--- Vitamin C/colors.h
+++ Vitamin C/colors.h
@@ -12,4 +12,16 @@ void init_colors( SDL_PixelFormat* pixel_format );
 // Make rgb color with r(0-255), g(0-255), and b(0-255).
 color_t make_rgb( uchar r,uchar g,uchar b );
 
+color_t color_white();
+color_t color_black();
+color_t color_gray();
+color_t color_light_gray();
+color_t color_dark_gray();
+color_t color_red();
+color_t color_yellow();
+color_t color_green();
+color_t color_cyan();
+color_t color_blue();
+color_t color_magenta();
+
 #endif
